80edb2291ce655a709f8681d21ebbcc7a4703e60
UmairFarooqLone <umairibnfarooq1@gmail.com>
2024-05-02 14:32:57 +0530

    Image AutoScroller

--- public/js/slick.js
+++ public/js/slick.js
@@ -22,3 +22,28 @@ $(".slick-slider").slick({
       }
     ]
   });
+
+  $(".slick-slider2").slick({
+    slidesToShow: 1,  //How Many Images at a time
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 1150,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+        }
+      },
+      {
+        breakpoint: 850,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+        }
+      }
+    ]
+  });
